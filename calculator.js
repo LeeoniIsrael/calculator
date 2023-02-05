df87daae280edb659c3c21d06d09a4b316851da4
Leeoni Israel <112006165+LeeoniIsrael@users.noreply.github.com>
2023-02-05 15:30:39 -0500

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -5,121 +5,160 @@ var num2 = "";
 var result ;
 
 var operation = "";
-// var operationDeclared=1;
+var operationDeclared=0;
 
 document.getElementById("numDiv").addEventListener("click", function(){
-    operation="division";
-    operationDeclared=0;
+    operation="divide";
+    operationDeclared=1;
+    result = num1;
   })
   
   document.getElementById("numMult").addEventListener("click", function(){
       operation="multiply";
-      operationDeclared=0;
+      operationDeclared=1;
     })
   
     document.getElementById("numAdd").addEventListener("click", function(){
       operation="add";
-      operationDeclared=0;
-      console.log("here",operationDeclared)
+      operationDeclared=1;
     })
   
     document.getElementById("numMinus").addEventListener("click", function(){
       operation="subtract";
-      operationDeclared=0;
+      operationDeclared=1;
     })
 //number 1
-if(operationDeclared==1)
-{
+
+   
     document.getElementById("num1").addEventListener("click", function(){
-         num1=num1+"1";
+     if(!operationDeclared){
+          num1=num1+"1";
+          document.getElementById("display").innerHTML = num1;
+     }
+     else{
+          num2=num2+"1";
+          document.getElementById("display").innerHTML = num2;
+     }
+        
     })
     document.getElementById("num2").addEventListener("click", function(){
-         num1=num1+"2";
+     if(!operationDeclared){
+          num1=num1+"2";
+          document.getElementById("display").innerHTML = num1;
+     }
+     else{
+          num2=num2+"2";
+          document.getElementById("display").innerHTML = num2;
+     }
+        
     })
     document.getElementById("num3").addEventListener("click", function(){
-         num1=num1+"3";
+     if(!operationDeclared){
+          num1=num1+"3";
+          document.getElementById("display").innerHTML = num1;
+     }
+     else{
+          num2=num2+"3";
+          document.getElementById("display").innerHTML = num2;
+     }
+        
     })
     document.getElementById("num4").addEventListener("click", function(){
-         num1=num1+"4";
+     if(!operationDeclared){
+          num1=num1+"4";
+          document.getElementById("display").innerHTML = num1;
+     }
+     else{
+          num2=num2+"4";
+          document.getElementById("display").innerHTML = num2;
+     }
+        
     })
     document.getElementById("num5").addEventListener("click", function(){
-         num1=num1+"5";
-         console.log("opertionDeclared",operationDeclared)
-         console.log("herenum1",num1)
+     if(!operationDeclared){
+          num1=num1+"5";
+          document.getElementById("display").innerHTML = num1;
+     }
+     else{
+          num2=num2+"5";
+          document.getElementById("display").innerHTML = num2;
+     }
+        
     })
     document.getElementById("num6").addEventListener("click", function(){
-         num1=num1+"6";
-         console.log("opertionDeclared",operationDeclared)
-         console.log("herenum1",num1)
+     if(!operationDeclared){
+          num1=num1+"6";
+          document.getElementById("display").innerHTML = num1;
+     }
+     else{
+          num2=num2+"6";
+          document.getElementById("display").innerHTML = num2;
+     }
+        
     })
     document.getElementById("num7").addEventListener("click", function(){
-         num1=num1+"7";
+     if(!operationDeclared){
+          num1=num1+"7";
+          document.getElementById("display").innerHTML = num1;
+     }
+     else{
+          num2=num2+"7";
+          document.getElementById("display").innerHTML = num2;
+     }
+        
     })
-    
     document.getElementById("num8").addEventListener("click", function(){
-         num1=num1+"8";
+     if(!operationDeclared){
+          num1=num1+"8";
+          document.getElementById("display").innerHTML = num1;
+     }
+     else{
+          num2=num2+"8";
+          document.getElementById("display").innerHTML = num2;
+     }
+        
     })
     document.getElementById("num9").addEventListener("click", function(){
-         num1=num1+"9";
+     if(!operationDeclared){
+          num1=num1+"9";
+          document.getElementById("display").innerHTML = num1;
+     }
+     else{
+          num2=num2+"9";
+          document.getElementById("display").innerHTML = num2;
+     }
+        
     })
-    
     document.getElementById("num0").addEventListener("click", function(){
-         num1=num1+"0";
+     if(!operationDeclared){
+          num1=num1+"0";
+          document.getElementById("display").innerHTML = num1;
+     }
+     else{
+          num2=num2+"0";
+          document.getElementById("display").innerHTML = num2;
+     }
+        
     })
-
-
-
-}
-
-
-
-// console.log("opertion declared",operationDeclared)
-if(operationDeclared==0)
-{
-    document.getElementById("num1").addEventListener("click", function(){
-         num2=num2+"1";
-    })
-    document.getElementById("num2").addEventListener("click", function(){
-         num2=num2+"2";
-    })
-    document.getElementById("num3").addEventListener("click", function(){
-         num2=num2+"3";
-    })
-    document.getElementById("num4").addEventListener("click", function(){
-         num2=num2+"4";
-    })
-    document.getElementById("num5").addEventListener("click", function(){
-         num2=num2+"5";
-         console.log("opertionDeclared",operationDeclared)
-         console.log("herenum2",num2)
-    })
-    document.getElementById("num6").addEventListener("click", function(){
-         num2=num2+"6";
-         console.log("opertionDeclared",operationDeclared)
-         console.log("herenum2",num2)
-    })
-    document.getElementById("num7").addEventListener("click", function(){
-         num2=num2+"7";
+    document.getElementById("numDecimal").addEventListener("click", function(){
+     if(!operationDeclared){
+          num1=num1+".";
+          document.getElementById("display").innerHTML = num1;
+     }
+     else{
+          num2=num2+".";
+          document.getElementById("display").innerHTML = num2;
+     }
+        
     })
     
-    document.getElementById("num8").addEventListener("click", function(){
-         num2=num2+"8";
-    })
-    document.getElementById("num9").addEventListener("click", function(){
-         num2=num2+"9";
-    })
-    
-    document.getElementById("num0").addEventListener("click", function(){
-         num2=num2+"0";
-    })
-}
 
 
 
-//number 2
 
 
-//operation
+
+
 
 
 function add(a, b)
@@ -142,13 +181,44 @@ function multiply(a, b)
     return a * b;
 }
 
+
 document.getElementById("numEquals").addEventListener("click", function()
 {
     if(operation=="add"){
      result=add(Number(num1),Number(num2))
-    // console
-    
-     console.log("result",num1,num2,operationDeclared)
+     console.log("result",num1,num2,result)
+     }
+     if(operation=="subtract"){
+          result=subtract(Number(num1),Number(num2))
+          console.log("result", num1,num2,result)
+     }
+     if(operation=="divide"){
+          result=divide(Number(num1),Number(num2))
+          console.log("result", num1,num2,result)
+     }
+     if(operation=="multiply"){
+          result=multiply(Number(num1),Number(num2))
+          console.log("result", num1,num2,result)
+     }
+     document.getElementById("display").innerHTML = result;
+     num1="";
+     num2="";
+     operation="";
+     operationDeclared=0;
 
-    }
 })
+document.getElementById("numClear").addEventListener("click", function()
+{
+     num1="";
+     num2="";
+     operation="";
+     operationDeclared=0;
+     document.getElementById("display").innerHTML = "";
+})
+
+
+
+
+
+
+
