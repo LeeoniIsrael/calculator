831202b851e5347d753e4369c2c15629022cf82b
Leeoni Israel <lisraeluofsc@gmail.com>
2023-02-19 14:24:03 -0500

    update #1

--- calculator.js
+++ calculator.js
@@ -6,6 +6,12 @@ var result ;
 
 var operation = "";
 var operationDeclared=0;
+<<<<<<< HEAD
+
+var a=document.getElementsByClassName("number")
+console.log(a)
+=======
+>>>>>>> efc2cc37ac60266bebc3883e998d4862aeb5e0aa
 
 document.getElementById("numDiv").addEventListener("click", function(){
     operation="divide";
@@ -30,6 +36,137 @@ document.getElementById("numDiv").addEventListener("click", function(){
 //number 1
 
    
+<<<<<<< HEAD
+//     document.getElementById("num1").addEventListener("click", function(){
+//      if(!operationDeclared){
+//           num1=num1+"1";
+//           document.getElementById("display").innerHTML = num1;
+//      }
+//      else{
+//           num2=num2+"1";
+//           document.getElementById("display").innerHTML = num2;
+//      }
+        
+//     })
+//     document.getElementById("num2").addEventListener("click", function(){
+//      if(!operationDeclared){
+//           num1=num1+"2";
+//           document.getElementById("display").innerHTML = num1;
+//      }
+//      else{
+//           num2=num2+"2";
+//           document.getElementById("display").innerHTML = num2;
+//      }
+        
+//     })
+//     document.getElementById("num3").addEventListener("click", function(){
+//      if(!operationDeclared){
+//           num1=num1+"3";
+//           document.getElementById("display").innerHTML = num1;
+//      }
+//      else{
+//           num2=num2+"3";
+//           document.getElementById("display").innerHTML = num2;
+//      }
+        
+//     })
+//     document.getElementById("num4").addEventListener("click", function(){
+//      if(!operationDeclared){
+//           num1=num1+"4";
+//           document.getElementById("display").innerHTML = num1;
+//      }
+//      else{
+//           num2=num2+"4";
+//           document.getElementById("display").innerHTML = num2;
+//      }
+        
+//     })
+//     document.getElementById("num5").addEventListener("click", function(){
+//      if(!operationDeclared){
+//           num1=num1+"5";
+//           document.getElementById("display").innerHTML = num1;
+//      }
+//      else{
+//           num2=num2+"5";
+//           document.getElementById("display").innerHTML = num2;
+//      }
+        
+//     })
+//     document.getElementById("num6").addEventListener("click", function(){
+//      if(!operationDeclared){
+//           num1=num1+"6";
+//           document.getElementById("display").innerHTML = num1;
+//      }
+//      else{
+//           num2=num2+"6";
+//           document.getElementById("display").innerHTML = num2;
+//      }
+        
+//     })
+//     document.getElementById("num7").addEventListener("click", function(){
+//      if(!operationDeclared){
+//           num1=num1+"7";
+//           document.getElementById("display").innerHTML = num1;
+//      }
+//      else{
+//           num2=num2+"7";
+//           document.getElementById("display").innerHTML = num2;
+//      }
+        
+//     })
+//     document.getElementById("num8").addEventListener("click", function(){
+//      if(!operationDeclared){
+//           num1=num1+"8";
+//           document.getElementById("display").innerHTML = num1;
+//      }
+//      else{
+//           num2=num2+"8";
+//           document.getElementById("display").innerHTML = num2;
+//      }
+        
+//     })
+//     document.getElementById("num9").addEventListener("click", function(){
+//      if(!operationDeclared){
+//           num1=num1+"9";
+//           document.getElementById("display").innerHTML = num1;
+//      }
+//      else{
+//           num2=num2+"9";
+//           document.getElementById("display").innerHTML = num2;
+//      }
+        
+//     })
+//     document.getElementById("num0").addEventListener("click", function(){
+//      if(!operationDeclared){
+//           num1=num1+"0";
+//           document.getElementById("display").innerHTML = num1;
+//      }
+//      else{
+//           num2=num2+"0";
+//           document.getElementById("display").innerHTML = num2;
+//      }
+        
+//     })
+
+
+var numbers=document.getElementsByClassName("number")
+for (i=0;i<numbers.length; i++){
+     numbers[i].addEventListener("click",function(){
+          if(!operationDeclared){
+               num1=num1+this.innerHTML;
+               document.getElementById("display").innerHTML = num1;
+          }
+          else
+          {
+               num2=num2+this.innerHTML;
+               document.getElementById("display").innerHTML = num2;
+          }
+     }
+     )  
+}
+
+
+=======
     document.getElementById("num1").addEventListener("click", function(){
      if(!operationDeclared){
           num1=num1+"1";
@@ -140,6 +277,7 @@ document.getElementById("numDiv").addEventListener("click", function(){
      }
         
     })
+>>>>>>> efc2cc37ac60266bebc3883e998d4862aeb5e0aa
     document.getElementById("numDecimal").addEventListener("click", function(){
      if(!operationDeclared){
           num1=num1+".";
